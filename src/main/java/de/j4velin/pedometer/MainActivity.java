40fe3850fbaaa4e7912476107323a3dc610983a6
Dennis Kao <d.kao@live.com>
2018-02-11 23:51:53 -0500

    Updated libraries to 27.0.2
    App now starts on a new (blank) activity page
    
    Implemented a fragment window and a nav bar
    
    Switching functionality of nav bar still needs to be implemented
    
    So does importing backend logic from the original Activity_Main.java

---
+++ src/main/java/de/j4velin/pedometer/MainActivity.java
@@ -0,0 +1,40 @@
+package de.j4velin.pedometer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.support.annotation.NonNull;
+import android.view.MenuItem;
+
+
+/**
+ * Created by dkao on 2/11/2018.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+            }
+            return true;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.navbar_fragments);
+
+//        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
