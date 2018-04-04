a474f46b188082e34b6374f15ced2875ede816f3
Dennis Kao <d.kao@live.com>
2018-04-04 19:38:32 -0400

    Implemented a sorting FAB and linking the appropriate sorting functions

--- src/main/java/de/j4velin/pedometer/ui/Fragment_StepHistory.java
+++ src/main/java/de/j4velin/pedometer/ui/Fragment_StepHistory.java
@@ -12,6 +12,10 @@ import android.view.ViewGroup;
 import android.widget.ListView;
 import android.support.design.widget.TabLayout;
 
+import com.github.clans.fab.FloatingActionMenu;
+import com.github.clans.fab.FloatingActionButton;
+
+
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -40,12 +44,20 @@ public class Fragment_StepHistory extends Fragment {
     private String mParam2;
 
     private OnFragmentInteractionListener mListener;
-
     private ListView lsView;
+    private TabLayout recordTypeTab;
 
     private ArrayList<DayStepHistory> dayHistoryRecords = null;
     private ArrayList<WeekStepHistory> weekHistoryRecords = null;
     private ArrayList<MonthStepHistory> monthHistoryRecords = null;
+
+    private FloatingActionMenu fabMenu;
+    private FloatingActionButton stepButton;
+    private FloatingActionButton dateButton;
+    private FloatingActionButton goalButton;
+    private FloatingActionButton stdDevButton;
+    private FloatingActionButton medianButton;
+    private FloatingActionButton bestDayButton;
 
     private StepHistoryCellAdapter stepHistoryCellAdapter = null;
 
@@ -74,6 +86,7 @@ public class Fragment_StepHistory extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -81,14 +94,105 @@ public class Fragment_StepHistory extends Fragment {
         setHasOptionsMenu(true);
     }
 
+    public void sortListener(String option) {
+
+        int currentTimeFrame = recordTypeTab.getSelectedTabPosition();
+
+        //  sort the appropriate ArrayList
+        switch (option) {
+            case "Step":
+                if (currentTimeFrame == 0) dayHistoryRecords.sort(DayStepHistory.StepComparator);
+                else if (currentTimeFrame == 1) weekHistoryRecords.sort(WeekStepHistory.StepComparator);
+                else monthHistoryRecords.sort(MonthStepHistory.StepComparator);
+                break;
+            case "Date":
+                if (currentTimeFrame == 0) dayHistoryRecords.sort(DayStepHistory.DayDateComparator);
+                else if (currentTimeFrame == 1) weekHistoryRecords.sort(WeekStepHistory.WeekDateComparator);
+                else monthHistoryRecords.sort(MonthStepHistory.MonthDateComparator);
+                break;
+            case "Goal":
+                if (currentTimeFrame == 0) dayHistoryRecords.sort(DayStepHistory.DayGoalComparator);
+                break;
+            case "Median":
+                if (currentTimeFrame == 1) weekHistoryRecords.sort(WeekStepHistory.WeekMedianComparator);
+                else monthHistoryRecords.sort(MonthStepHistory.MonthMedianComparator);
+                break;
+            case "Best Day":
+                if (currentTimeFrame == 1) weekHistoryRecords.sort(WeekStepHistory.WeekBestDayComparator);
+                else monthHistoryRecords.sort(MonthStepHistory.MonthBestDayComparator);
+                break;
+            case "Std. Dev.":
+                if (currentTimeFrame == 1) weekHistoryRecords.sort(WeekStepHistory.WeekStdDevComparator);
+                else monthHistoryRecords.sort(MonthStepHistory.MonthStdDevComparator);
+                break;
+            default:
+                break;
+        }
+
+        //  feed to adapter and change the view
+        if (currentTimeFrame == 0)  {
+            this.stepHistoryCellAdapter = new StepHistoryCellAdapter(getContext(), this.dayHistoryRecords);
+        }
+        else if (currentTimeFrame == 1) {
+            this.stepHistoryCellAdapter = new StepHistoryCellAdapter(getContext(), this.weekHistoryRecords);
+        }
+        else {
+            this.stepHistoryCellAdapter = new StepHistoryCellAdapter(getContext(), this.monthHistoryRecords);
+        }
+
+        this.lsView.setAdapter(this.stepHistoryCellAdapter);
+    }
+
+    public FloatingActionButton setupButton(String buttonText) {
+        final FloatingActionButton newButton = new FloatingActionButton((getContext()));
+        newButton.setLabelText(buttonText);
+        newButton.setButtonSize(FloatingActionButton.SIZE_MINI);
+        newButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sortListener(newButton.getLabelText());
+            }
+        });
+
+        return newButton;
+    }
+
+    public void setOptions(String timeFrameSelected) {
+
+        fabMenu.removeAllMenuButtons();
+
+        //  cells are always sortable by Date and Steps
+        fabMenu.addMenuButton(stepButton);
+        fabMenu.addMenuButton(dateButton);
+
+        if (timeFrameSelected.equals("day")) {
+            fabMenu.addMenuButton(goalButton);
+        }
+        else if ((timeFrameSelected.equals("week")) || (timeFrameSelected.equals("month"))) {
+            fabMenu.addMenuButton(stdDevButton);
+            fabMenu.addMenuButton(medianButton);
+            fabMenu.addMenuButton(bestDayButton);
+        }
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view =  inflater.inflate(R.layout.fragment__step_history , container, false);
-        TabLayout recordTypeTab = view.findViewById(R.id.tab_layout);
+        recordTypeTab = view.findViewById(R.id.tab_layout);
         this.lsView = view.findViewById(R.id.step_history_list);
 
         recordTypeTab.addOnTabSelectedListener(new StepHistoryFragmentListener(this));
         this.showDayStepHistory();
+
+        fabMenu = (FloatingActionMenu) view.findViewById(R.id.fab);
+        stepButton = setupButton("Step");
+        dateButton = setupButton("Date");
+        goalButton = setupButton("Goal");
+        stdDevButton = setupButton("Std. Dev.");
+        medianButton = setupButton("Median");
+        bestDayButton = setupButton("Best Day");
+
+        setOptions("day");
         return view;
     }
 
@@ -105,6 +209,8 @@ public class Fragment_StepHistory extends Fragment {
         else{
             this.lsView.setAdapter(this.stepHistoryCellAdapter);
         }
+
+        if (fabMenu != null) setOptions("day");
     }
 
     public void showWeekStepHistory() {
@@ -123,6 +229,8 @@ public class Fragment_StepHistory extends Fragment {
         } else {
             this.lsView.setAdapter(this.stepHistoryCellAdapter);
         }
+
+        if (fabMenu != null) setOptions("week");
     }
 
     public void showMonthStepHistory() {
@@ -140,6 +248,8 @@ public class Fragment_StepHistory extends Fragment {
         else{
             this.lsView.setAdapter(this.stepHistoryCellAdapter);
         }
+
+        if (fabMenu != null) setOptions("month");
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -180,5 +290,4 @@ public class Fragment_StepHistory extends Fragment {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
-
 }
